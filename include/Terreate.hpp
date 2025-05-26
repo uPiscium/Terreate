bf6c03d87d79a0f2e6ae4e2871ec3771e02ee4b1
uPiscium <upiscium@gmail.com>
2025-05-26 22:26:49 +0900

    Delete unused files and directories.

--- include/Terreate.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <core/context.hpp>
-#include <core/instance.hpp>
-#include <util/debugger.hpp>
-
-namespace Terreate {
-;
-}
--- include/core/IDisposable.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Terreate::Core {
-class IDisposable {
-public:
-  virtual ~IDisposable() = default;
-  virtual void dispose() = 0;
-};
-} // namespace Terreate::Core
