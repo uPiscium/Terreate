d51ef7524aeb112383c97b2af755f3c4a213bb96
uPiscium <upiscium@gmail.com>
2025-05-26 22:33:15 +0900

    Add device implementation

--- include/core/device.hpp
+++ include/core/device.hpp
@@ -1,25 +1,58 @@
 #pragma once
-
-#include <core/IDisposable.hpp>
-#include <core/api.hpp>
-#include <type.hpp>
+#include "../decl/type.hpp"
 
 namespace Terreate::Core {
-using namespace Terreate::Type;
+struct QueueFamilyIndices {
+  Type::optional<Type::u32> graphicsFamily;
+  Type::optional<Type::u32> presentFamily;
 
-class Device : public IDisposable {
+  operator Type::set<Type::u32>() const {
+    return {graphicsFamily.value_or(0), presentFamily.value_or(0)};
+  }
+  operator bool() const {
+    return graphicsFamily.has_value() && presentFamily.has_value();
+  }
+};
+
+struct SwapchainSupportDetails {
+  VkSurfaceCapabilitiesKHR capabilities;
+  Type::vec<VkSurfaceFormatKHR> formats;
+  Type::vec<VkPresentModeKHR> presentModes;
+};
+
+class Device {
 private:
+  PROHIBIT_COPY_AND_ASSIGN(Device);
+
+private:
+  VkInstance mInstance = VK_NULL_HANDLE;
+
+  QueueFamilyIndices mQueueFamily;
   VkPhysicalDevice mPhysicalDevice = VK_NULL_HANDLE;
   VkDevice mDevice = VK_NULL_HANDLE;
 
 private:
-  int rateDevice(VkPhysicalDevice device) const;
-  void pickPhysicalDevice(VkInstance instance);
+  QueueFamilyIndices findQueue(VkPhysicalDevice device,
+                               VkSurfaceKHR surface) const;
+  int rateDevice(VkPhysicalDevice device, VkSurfaceKHR surface) const;
+  bool checkExtSupport(VkPhysicalDevice device);
+  SwapchainSupportDetails getSwapchainSupport(VkPhysicalDevice device,
+                                              VkSurfaceKHR surface);
+  void pickPhysicalDevice(VkSurfaceKHR surface);
+  void createLogicalDevice(VkSurfaceKHR surface);
 
 public:
-  Device(VkInstance instance);
-  ~Device();
+  Device(VkInstance instance, VkSurfaceKHR surface);
+  ~Device() { this->dispose(); }
 
-  void dispose() override;
+  QueueFamilyIndices getQueue() const { return mQueueFamily; }
+  SwapchainSupportDetails getSwapchainSupport(VkSurfaceKHR surface) {
+    return this->getSwapchainSupport(mPhysicalDevice, surface);
+  }
+
+  void dispose();
+
+  operator VkDevice() const { return mDevice; }
+  operator VkPhysicalDevice() const { return mPhysicalDevice; }
 };
 } // namespace Terreate::Core
