dfa372224f4e8ee4c608252c483fe26e0cf29e2e
uPiscium <upiscium@gmail.com>
2025-06-27 18:04:17 +0900

    DELETE ALL FILES

--- tests/resources/shader/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd tests/resources/shader/ || return
-glslc -o vert.spv -fshader-stage=vertex vert.glsl
-glslc -o frag.spv -fshader-stage=fragment frag.glsl
-cd ../../../ || return
