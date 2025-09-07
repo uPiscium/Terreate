0ceb05b7b1d0cbcdbe80ee72606e6400bb1a4883
uPiscium <upiscium@gmail.com>
2025-09-07 22:19:04 +0900

    Impl tutorial and CMakeLists.txt for Vulkan support

--- example/tutorial/include/main.hpp
+++ example/tutorial/include/main.hpp
@@ -1,79 +1,62 @@
-#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
 #include <vulkan/vulkan.hpp>
 
-#include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
-typedef uint32_t u32;
 typedef std::string str;
+typedef uint32_t u32;
 
 template <typename T> using vec = std::vector<T>;
 
-#define TERREATE_ENGINE_VERSION_MAJOR 0
-#define TERREATE_ENGINE_VERSION_MINOR 1
-#define TERREATE_ENGINE_VERSION_PATCH 0
-#define TERREATE_ENGINE_VERSION                                                \
-  VK_MAKE_VERSION(TERREATE_ENGINE_VERSION_MAJOR,                               \
-                  TERREATE_ENGINE_VERSION_MINOR,                               \
-                  TERREATE_ENGINE_VERSION_PATCH)
+static constexpr char const *ENGINE_NAME = "Terreate";
+static constexpr int WINDOW_WIDTH = 800;
+static constexpr int WINDOW_HEIGHT = 600;
+static constexpr u32 ENGINE_VERSION = VK_MAKE_VERSION(0, 1, 0);
 
-static vec<char const *> const ENABLED_INSTANCE_LAYERS = {
+static constexpr char const *VALIDATION_LAYERS[] = {
     "VK_LAYER_KHRONOS_validation"};
 
-struct QueueFamilyIndices {
-  u32 graphicsFamily = UINT32_MAX;
-  // u32 presentFamily = UINT32_MAX;
-
-  bool isComplete() const {
-    return graphicsFamily != UINT32_MAX; //&& presentFamily != UINT32_MAX;
-  }
-};
-
-class VulkanTriangle {
+class VulkanTutorial {
 private:
+  bool mDebugMode = false;
+
+  VkInstance mInstance = VK_NULL_HANDLE;
+  VkDebugUtilsMessengerEXT mDebugMessenger = VK_NULL_HANDLE;
+
   SDL_Window *mWindow = nullptr;
-  vk::UniqueInstance mInstance;
-  vk::UniqueDebugUtilsMessengerEXT mDebugMessenger;
-  vk::PhysicalDevice mPhysicalDevice;
-  vk::UniqueDevice mDevice;
 
-#ifdef TERREATE_DEBUG_BUILD
 private:
-  bool isLayerSupported(vec<char const *> const &layers) const;
+  void initWindow();
+  bool checkValidationLayerSupport();
+  vec<char const *> getRequiredExtensions();
+  void populateDebugMessengerCreateInfo(
+      VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+  void createInstance(str const &appName, u32 const &appVersion);
+  VkResult createDebugUtilsMessengerEXT(
+      VkDebugUtilsMessengerCreateInfoEXT const *pCreateInfo,
+      VkAllocationCallbacks const *pAllocator);
+  VkResult
+  destroyDebugUtilsMessengerEXT(VkAllocationCallbacks const *pAllocator);
   void setupDebugMessenger();
-
-public:
-  static VKAPI_ATTR VkBool32 VKAPI_CALL
-  debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                vk::DebugUtilsMessageTypeFlagsEXT messageType,
-                vk::DebugUtilsMessengerCallbackDataEXT const *pCallbackData,
-                void *pUserData);
-  static void populateDebugMessengerCreateInfo(
-      vk::DebugUtilsMessengerCreateInfoEXT &info,
-      vk::PFN_DebugUtilsMessengerCallbackEXT callback);
-#endif
-
-private:
-  void initSDL() const;
-  SDL_Window *initWindow(u32 const &width, u32 const &height,
-                         str const &title) const;
-  vec<char const *> getRequiredExtensions() const;
-  vk::UniqueInstance createInstance(str const &appName,
-                                    u32 const &appVersion) const;
-  QueueFamilyIndices findQueueFamily(vk::PhysicalDevice const &device) const;
-  int rateDevice(vk::PhysicalDevice const &device) const;
-  vk::PhysicalDevice
-  pickPhysicalDevice(vk::UniqueInstance const &instance) const;
   void initVulkan();
+  bool pollEvents();
   void mainLoop();
   void cleanup();
 
 public:
+  static VKAPI_ATTR VkBool32 VKAPI_CALL
+  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT messageType,
+                VkDebugUtilsMessengerCallbackDataEXT const *pCallbackData,
+                void *pUserData);
+
+public:
+  VulkanTutorial(bool debug = false) : mDebugMode(debug) {}
   void run();
 };
 
--- example/tutorial/src/main.cpp
+++ example/tutorial/src/main.cpp
@@ -1,47 +1,40 @@
 #include "../include/main.hpp"
-VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE;
 
-#ifdef TERREATE_DEBUG_BUILD
-bool VulkanTriangle::isLayerSupported(vec<char const *> const &layers) const {
-  if (layers.empty()) {
-    return true; // No layers to check, considered supported
+void VulkanTutorial::initWindow() {
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_JOYSTICK |
+                SDL_INIT_GAMEPAD | SDL_INIT_HAPTIC | SDL_INIT_CAMERA |
+                SDL_INIT_SENSOR | SDL_INIT_AUDIO)) {
+    throw std::runtime_error("Failed to initialize SDL.");
+    return;
   }
 
-  u32 layerCount = 0;
-  vk::Result result =
-      vk::enumerateInstanceLayerProperties(&layerCount, nullptr);
-  if (result != vk::Result::eSuccess) {
-    std::cerr << "Failed to get instance layer count: " << vk::to_string(result)
-              << std::endl;
-    return false;
-  }
+  mWindow = SDL_CreateWindow("Vulkan Tutorial", WINDOW_WIDTH, WINDOW_HEIGHT,
+                             SDL_WINDOW_VULKAN);
+}
 
-  if (layerCount == 0) {
-    std::cerr << "No Vulkan layers available." << std::endl;
-    return false;
-  }
+bool VulkanTutorial::checkValidationLayerSupport() {
+  u32 layerCount;
+  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
-  vec<vk::LayerProperties> availableLayers(layerCount);
-  result =
-      vk::enumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+  std::vector<VkLayerProperties> availableLayers(layerCount);
+  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-  if (result != vk::Result::eSuccess) {
-    std::cerr << "Failed to enumerate instance layers: "
-              << vk::to_string(result) << std::endl;
-    return false;
-  }
+  // std::cout << "Available Vulkan layers:\n";
+  // for (const auto &layer : availableLayers) {
+  //   std::cout << '\t' << layer.layerName << '\n';
+  // }
 
-  for (auto const &layerName : layers) {
-    bool found = false;
-    for (auto const &layer : availableLayers) {
-      if (std::strcmp(layer.layerName, layerName) == 0) {
-        found = true;
+  for (auto const &layer : VALIDATION_LAYERS) {
+    bool layerFound = false;
+
+    for (const auto &availableLayer : availableLayers) {
+      if (std::strcmp(layer, availableLayer.layerName) == 0) {
+        layerFound = true;
         break;
       }
     }
 
-    if (!found) {
-      std::cerr << "Layer not supported: " << layerName << std::endl;
+    if (!layerFound) {
       return false;
     }
   }
@@ -49,50 +42,190 @@ bool VulkanTriangle::isLayerSupported(vec<char const *> const &layers) const {
   return true;
 }
 
-void VulkanTriangle::populateDebugMessengerCreateInfo(
-    vk::DebugUtilsMessengerCreateInfoEXT &info,
-    vk::PFN_DebugUtilsMessengerCallbackEXT callback) {
-  vk::DebugUtilsMessageSeverityFlagsEXT severity =
-      vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
-      vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
-      vk::DebugUtilsMessageSeverityFlagBitsEXT::eError;
-  vk::DebugUtilsMessageTypeFlagsEXT type =
-      vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
-      vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
-      vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance;
-  info = vk::DebugUtilsMessengerCreateInfoEXT{
-      {}, severity, type, callback, nullptr};
+vec<char const *> VulkanTutorial::getRequiredExtensions() {
+  u32 extensionCount = 0;
+  char const *const *extensions =
+      SDL_Vulkan_GetInstanceExtensions(&extensionCount);
+  if (!extensions) {
+    throw std::runtime_error("Failed to get required Vulkan extensions.");
+  }
+  vec<char const *> requiredExtensions(extensions, extensions + extensionCount);
+
+  if (mDebugMode) {
+    requiredExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+  }
+
+  return requiredExtensions;
 }
 
-void VulkanTriangle::setupDebugMessenger() {
-  vk::DebugUtilsMessengerCreateInfoEXT createInfo;
-  VulkanTriangle::populateDebugMessengerCreateInfo(
-      createInfo, VulkanTriangle::debugCallback);
+void VulkanTutorial::populateDebugMessengerCreateInfo(
+    VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
+  createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+  createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                               VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+  createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                           VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+  createInfo.pfnUserCallback = debugCallback;
+  createInfo.pUserData = nullptr; // Optional
+}
 
-  try {
-    mDebugMessenger = mInstance->createDebugUtilsMessengerEXTUnique(createInfo);
-  } catch (vk::SystemError const &e) {
-    std::cerr << "Failed to create debug messenger: " << e.what() << std::endl;
-    throw std::runtime_error("Failed to create debug messenger");
+void VulkanTutorial::createInstance(str const &appName, u32 const &appVersion) {
+  if (mDebugMode && !this->checkValidationLayerSupport()) {
+    throw std::runtime_error("Validation layers requested, but not available.");
+  }
+
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = appName.c_str();
+  appInfo.applicationVersion = appVersion;
+  appInfo.pEngineName = ENGINE_NAME;
+  appInfo.engineVersion = ENGINE_VERSION;
+  appInfo.apiVersion = VK_API_VERSION_1_4;
+
+  vec<char const *> extensions = this->getRequiredExtensions();
+
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+  createInfo.enabledExtensionCount = extensions.size();
+  createInfo.ppEnabledExtensionNames = extensions.data();
+
+  VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+  if (mDebugMode) {
+    createInfo.enabledLayerCount =
+        sizeof(VALIDATION_LAYERS) / sizeof(VALIDATION_LAYERS[0]);
+    createInfo.ppEnabledLayerNames = VALIDATION_LAYERS;
+
+    this->populateDebugMessengerCreateInfo(debugCreateInfo);
+    createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo;
+  } else {
+    createInfo.enabledLayerCount = 0;
+    createInfo.ppEnabledLayerNames = nullptr;
+  }
+
+  u32 extCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
+  vec<VkExtensionProperties> availableExtensions(extCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extCount,
+                                         availableExtensions.data());
+
+  // std::cout << "Available Vulkan extensions:\n";
+  // for (const auto &extension : availableExtensions) {
+  //   std::cout << '\t' << extension.extensionName << '\n';
+  // }
+
+  if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to create Vulkan instance.");
   }
 }
 
-VKAPI_ATTR VkBool32 VKAPI_CALL VulkanTriangle::debugCallback(
-    vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-    vk::DebugUtilsMessageTypeFlagsEXT messageType,
-    vk::DebugUtilsMessengerCallbackDataEXT const *pCallbackData,
+VkResult VulkanTutorial::createDebugUtilsMessengerEXT(
+    VkDebugUtilsMessengerCreateInfoEXT const *pCreateInfo,
+    VkAllocationCallbacks const *pAllocator) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      mInstance, "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    return func(mInstance, pCreateInfo, nullptr, &mDebugMessenger);
+  } else {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+VkResult VulkanTutorial::destroyDebugUtilsMessengerEXT(
+    VkAllocationCallbacks const *pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      mInstance, "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr && mDebugMessenger != VK_NULL_HANDLE) {
+    func(mInstance, mDebugMessenger, nullptr);
+    mDebugMessenger = VK_NULL_HANDLE;
+    return VK_SUCCESS;
+  } else {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+void VulkanTutorial::setupDebugMessenger() {
+  if (!mDebugMode) {
+    return;
+  }
+
+  VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+  this->populateDebugMessengerCreateInfo(createInfo);
+
+  if (this->createDebugUtilsMessengerEXT(&createInfo, nullptr) != VK_SUCCESS) {
+    throw std::runtime_error("Failed to set up debug messenger.");
+  }
+}
+
+void VulkanTutorial::initVulkan() {
+  this->createInstance("Vulkan Tutorial", VK_MAKE_VERSION(0, 1, 0));
+  this->setupDebugMessenger();
+}
+
+bool VulkanTutorial::pollEvents() {
+  SDL_Event handle;
+  while (SDL_PollEvent(&handle)) {
+    switch (handle.type) {
+    case SDL_EVENT_QUIT:
+      return false;
+
+    case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+    case SDL_EVENT_WINDOW_DESTROYED:
+      return false;
+
+    default:
+      break;
+    }
+  }
+
+  return true;
+}
+
+void VulkanTutorial::mainLoop() {
+  bool loop = true;
+  while (loop) {
+    loop = this->pollEvents();
+  }
+}
+
+void VulkanTutorial::cleanup() {
+  if (mDebugMode) {
+    this->destroyDebugUtilsMessengerEXT(nullptr);
+  }
+
+  if (mInstance != VK_NULL_HANDLE) {
+    vkDestroyInstance(mInstance, nullptr);
+    mInstance = VK_NULL_HANDLE;
+  }
+
+  if (mWindow) {
+    SDL_DestroyWindow(mWindow);
+    mWindow = nullptr;
+  }
+
+  SDL_Quit();
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL VulkanTutorial::debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    VkDebugUtilsMessengerCallbackDataEXT const *pCallbackData,
     void *pUserData) {
   switch (messageSeverity) {
-  case vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose:
-    std::cout << "VERBOSE: ";
+  case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+    std::cerr << "VERBOSE: ";
     break;
-  case vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo:
-    std::cout << "INFO: ";
+  case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+    std::cerr << "INFO: ";
     break;
-  case vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning:
+  case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
     std::cerr << "WARNING: ";
     break;
-  case vk::DebugUtilsMessageSeverityFlagBitsEXT::eError:
+  case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
     std::cerr << "ERROR: ";
     break;
   default:
@@ -102,242 +235,21 @@ VKAPI_ATTR VkBool32 VKAPI_CALL VulkanTriangle::debugCallback(
   std::cerr << pCallbackData->pMessage << std::endl;
   return VK_FALSE;
 }
-#endif
 
-void VulkanTriangle::initSDL() const {
-  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_JOYSTICK |
-                SDL_INIT_GAMEPAD | SDL_INIT_HAPTIC | SDL_INIT_CAMERA |
-                SDL_INIT_SENSOR | SDL_INIT_AUDIO)) {
-    std::cerr << "Failed to initialize SDL: " + str(SDL_GetError())
-              << std::endl;
-    return;
-  }
-}
-
-SDL_Window *VulkanTriangle::initWindow(u32 const &width, u32 const &height,
-                                       str const &title) const {
-  this->initSDL();
-  return SDL_CreateWindow(title.c_str(), width, height, SDL_WINDOW_VULKAN);
-}
-
-vec<char const *> VulkanTriangle::getRequiredExtensions() const {
-  u32 extCount = 0;
-  auto exts = SDL_Vulkan_GetInstanceExtensions(&extCount);
-  vec<char const *> extensions(exts, exts + extCount);
-
-#ifdef TERREATE_DEBUG_BUILD
-  // Add debug report extension if in debug mode
-  extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-#endif
-
-  return extensions;
-}
-
-vk::UniqueInstance VulkanTriangle::createInstance(str const &appName,
-                                                  u32 const &appVersion) const {
-  vk::ApplicationInfo appInfo(appName.c_str(), appVersion, "Terreate",
-                              TERREATE_ENGINE_VERSION, vk::ApiVersion14);
-
-  vec<char const *> exts = this->getRequiredExtensions();
-
-  // TODO: Support VK_ERROR_INCOMPATIBLE_DRIVER
-  vk::InstanceCreateInfo createInfo;
-
-#ifdef TERREATE_DEBUG_BUILD
-  if (!this->isLayerSupported(ENABLED_INSTANCE_LAYERS)) {
-    std::cerr << "Validation layer not supported." << std::endl;
-    throw std::runtime_error("Validation layer not supported");
-  }
-
-  vk::DebugUtilsMessengerCreateInfoEXT debugCreateInfo;
-  VulkanTriangle::populateDebugMessengerCreateInfo(
-      debugCreateInfo, VulkanTriangle::debugCallback);
-
-  createInfo = vk::InstanceCreateInfo(
-      {}, &appInfo, ENABLED_INSTANCE_LAYERS.size(),
-      (char const *const *)ENABLED_INSTANCE_LAYERS.data(), exts.size(),
-      exts.data(), (vk::DebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo);
-#else
-  createInfo =
-      vk::InstanceCreateInfo({}, &appInfo, {}, {}, exts.size(), exts.data());
-#endif
-
-  vk::UniqueInstance instance;
-  try {
-    instance = vk::createInstanceUnique(createInfo);
-  } catch (vk::SystemError const &e) {
-    std::cerr << "Failed to create Vulkan instance: " << e.what() << std::endl;
-    throw std::runtime_error("Failed to create Vulkan instance");
-  }
-
-  if (!mInstance) {
-    std::cerr << "Failed to create Vulkan instance." << std::endl;
-    throw std::runtime_error("Failed to create Vulkan instance");
-  }
-
-  VULKAN_HPP_DEFAULT_DISPATCHER.init(mInstance.get());
-  return instance;
-}
-
-QueueFamilyIndices
-VulkanTriangle::findQueueFamily(vk::PhysicalDevice const &device) const {
-  QueueFamilyIndices indices;
-
-  vec<vk::QueueFamilyProperties> queueFamilies =
-      device.getQueueFamilyProperties();
-
-  int i = 0;
-  for (auto const &family : queueFamilies) {
-    if (family.queueFlags & vk::QueueFlagBits::eGraphics) {
-      indices.graphicsFamily = i;
-    }
-
-    if (indices.isComplete()) {
-      break;
-    }
-
-    ++i;
-  }
-
-  return indices;
-}
-
-int VulkanTriangle::rateDevice(vk::PhysicalDevice const &device) const {
-  vk::PhysicalDeviceProperties property = device.getProperties();
-  vk::PhysicalDeviceFeatures features = device.getFeatures();
-
-  if (!this->findQueueFamily(device).isComplete()) {
-    return 0; // Required queue families not found
-  }
-
-  if (property.deviceType != vk::PhysicalDeviceType::eDiscreteGpu &&
-      property.deviceType != vk::PhysicalDeviceType::eIntegratedGpu) {
-    return 0; // Not a suitable device type
-  }
-
-  // if (!features.geometryShader || !features.tessellationShader ||
-  //     !features.shaderStorageImageWriteWithoutFormat) {
-  //   return 0; // Required features not supported
-  // }
-
-  if (property.apiVersion < vk::ApiVersion14) {
-    std::cerr << "Device API version is too low" << std::endl;
-    return 0; // Device API version is too low
-  }
-
-  int score = 0;
-  score += property.deviceType == vk::PhysicalDeviceType::eDiscreteGpu ? 100
-           : property.deviceType == vk::PhysicalDeviceType::eIntegratedGpu
-               ? 50
-               : 0; // Score based on device type
-  score += property.limits.maxImageDimension2D >= 8192 ? 20
-           : property.limits.maxImageDimension2D >= 4096
-               ? 10
-               : 0; // Score based on max image dimension
-  score += property.limits.maxComputeWorkGroupCount[0] >= 1024 ? 20
-           : property.limits.maxComputeWorkGroupCount[0] >= 512
-               ? 10
-               : 0; // Score based on max compute work group count
-  score += property.limits.maxComputeWorkGroupSize[0] >= 256 ? 20
-           : property.limits.maxComputeWorkGroupSize[0] >= 128
-               ? 10
-               : 0; // Score based on max compute work group size
-  score += property.limits.maxComputeSharedMemorySize >= 65536 ? 20
-           : property.limits.maxComputeSharedMemorySize >= 32768
-               ? 10
-               : 0; // Score based on max compute shared memory size
-
-  score += features.geometryShader ? 10 : 0;     // Score if geometry shader is
-                                                 // supported
-  score += features.tessellationShader ? 10 : 0; // Score if tess
-                                                 // shader is supported
-  score += features.shaderStorageImageMultisample ? 10 : 0; // Score if
-  // shader storage image multisample is supported
-  score += features.shaderStorageImageReadWithoutFormat ? 10 : 0; // Score if
-  // shader storage image read without format is supported
-  score += features.shaderStorageImageWriteWithoutFormat ? 10 : 0; // Score if
-  // shader storage image write without format is supported
-
-  return score;
-}
-
-vk::PhysicalDevice
-VulkanTriangle::pickPhysicalDevice(vk::UniqueInstance const &instance) const {
-  vec<vk::PhysicalDevice> physicalDevices =
-      instance->enumeratePhysicalDevices();
-
-  if (physicalDevices.empty()) {
-    std::cerr << "No Vulkan physical devices found." << std::endl;
-    throw std::runtime_error("No Vulkan physical devices found");
-  }
-
-  int bestScore = -1;
-  vk::PhysicalDevice bestDevice;
-  for (auto const &device : physicalDevices) {
-    int score = this->rateDevice(device);
-    if (score > bestScore) {
-      bestScore = score;
-      bestDevice = device;
-    }
-  }
-
-  if (!bestDevice) {
-    std::cerr << "No suitable Vulkan physical device found." << std::endl;
-    throw std::runtime_error("No suitable Vulkan physical device found");
-  }
-
-  return bestDevice;
-}
-
-void VulkanTriangle::initVulkan() {
-  VULKAN_HPP_DEFAULT_DISPATCHER.init();
-  mInstance = this->createInstance("Vulkan Triangle", TERREATE_ENGINE_VERSION);
-  this->setupDebugMessenger();
-}
-
-void VulkanTriangle::mainLoop() {
-  while (true) {
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      switch (e.type) {
-      case SDL_EVENT_QUIT:
-        return;
-      case SDL_EVENT_WINDOW_CLOSE_REQUESTED: {
-        SDL_DestroyWindow(mWindow);
-        mWindow = nullptr;
-        return;
-      }
-      default:
-        break;
-      }
-    }
-  }
-}
-
-void VulkanTriangle::cleanup() {
-  if (mWindow) {
-    SDL_DestroyWindow(mWindow);
-    mWindow = nullptr;
-  }
-  SDL_Quit();
-}
-
-void VulkanTriangle::run() {
-  mWindow = this->initWindow(800, 600, "Vulkan Triangle");
+void VulkanTutorial::run() {
+  this->initWindow();
   this->initVulkan();
-  // this->mainLoop();
+  this->mainLoop();
   this->cleanup();
 }
 
 int main() {
-  VulkanTriangle app;
+  VulkanTutorial app(true);
+
   try {
     app.run();
-  } catch (std::exception const &e) {
-    std::cerr << "Exception: " << e.what() << std::endl;
-    return EXIT_FAILURE;
-  } catch (...) {
-    std::cerr << "Unknown exception occurred." << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
   }
 
