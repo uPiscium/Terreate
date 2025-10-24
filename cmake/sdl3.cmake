b63631acc71e4ef8e5ea50ccfb1b5bad3173400a
uPiscium <upiscium@gmail.com>
2025-10-24 11:09:47 +0900

    Move examples to separate repository and update SDL3 cmake module

--- cmake/sdl3.cmake
+++ cmake/sdl3.cmake
@@ -1,24 +1,25 @@
-include(FetchContent)
+function(prepare_sdl3)
+  find_package(SDL3 QUIET)
+  if(SDL3_FOUND)
+    message(STATUS "[SYSTEM] Found installed SDL3.")
+    return()
+  else()
+    message(STATUS "[SYSTEM] No installed SDL3 found, fetching...")
+  endif()
 
-find_package(SDL3 QUIET)
-if(SDL3_FOUND)
-  message(STATUS "[SDL3] Found installed version.")
-  return()
-else()
-  message(STATUS "[SDL3] No installed version found, fetching...")
-endif()
+  message(STATUS "[SYSTEM] Setting up...")
+  set(SDL_SHARED OFF)
+  set(SDL_INTERFACE ON)
+  set(SDL_STATIC ON)
 
-message(STATUS "[SDL3] Setting up...")
-set(SDL_SHARED OFF)
-set(SDL_INTERFACE ON)
-set(SDL_STATIC ON)
-FetchContent_Declare(
-  SDL3
-  GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-  GIT_TAG release-3.2.24
-  OVERRIDE_FIND_PACKAGE
-)
-FetchContent_MakeAvailable(SDL3)
-set(SDL3_DIR "${sdl3_BINARY_DIR}")
-message(STATUS "[SDL3] Ready.")
-
+  include(FetchContent)
+  FetchContent_Declare(
+    SDL3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG release-3.2.24
+    OVERRIDE_FIND_PACKAGE
+  )
+  FetchContent_MakeAvailable(SDL3)
+  set(SDL3_DIR "${sdl3_BINARY_DIR}")
+  message(STATUS "[SYSTEM] SDL3 Ready.")
+endfunction()
